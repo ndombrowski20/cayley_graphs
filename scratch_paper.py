ee5e843ffd57b0b30fdf5f4173c0cf92a2d88b90
natebook <ndombrowski20@gmail.com>
2018-08-31 12:08:41 -0400

    12:08
    from g3 4_8 to g3 4_10

--- scratch_paper.py
+++ scratch_paper.py
@@ -44,12 +44,12 @@ Ba = Word([B, a])
 B_word = Word([B])
 ba = Word([b, a])
 
-G = NewGroup([a, b], [aa, bbbbbb, aA, bB, abab], 8)
+G = NewGroup([a, b], [aa, bbbbbb, aA, bB, abab], 10)
 H = nx.DiGraph()
 
 elements = [identity]
 
-non_e_elements = G.yield_elems_of_quotient(4, 8)
+non_e_elements = G.yield_elems_of_quotient(4, 10)
 for entry in non_e_elements:
     elements.append(entry)
 
@@ -66,7 +66,7 @@ for member in elements:
         elem_with_letter.add_word(member)
         elem_with_letter.add_letter(gen_letter)
         for member_2 in elements:
-            if G.test_equals(elem_with_letter, member_2, 8):
+            if G.test_equals(elem_with_letter, member_2, 10):
                 H.add_edges_from([(member, member_2)], color=color_list[j])
                 break
 
@@ -88,7 +88,7 @@ pos = nx.spring_layout(H)
 
 nx.draw(H, pos, **options, labels=word_labels, edge_color=colors)
 
-numbers = open("products/g3_4_8.txt", 'w')
+numbers = open("products/g3_4_10.txt", 'w')
 
 numbers.write(str(H.number_of_nodes()) + " is the number of nodes\n")
 numbers.write(str(H.number_of_edges()) + " is the number of edges\n")
@@ -100,6 +100,6 @@ numbers.close()
 #     print(node)
 # for (u, v) in edges:
 #     print(u.return_word_str(), v.return_word_str())
-imagename = "products/g3_4_8.png"
+imagename = "products/g3_4_10.png"
 
 plt.savefig(imagename)
