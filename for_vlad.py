bace07a554e9172398b8674cab44aa4ce52cd639
natebook <ndombrowski20@gmail.com>
2018-08-31 11:18:38 -0400

    11:18
    time for g4 4_6

--- for_vlad.py
+++ for_vlad.py
@@ -44,12 +44,12 @@ Ba = Word([B, a])
 B_word = Word([B])
 ba = Word([b, a])
 
-G = NewGroup([a, b], [aa, bbb, aA, bB, ababab], 12)
+G = NewGroup([a, b], [aa, bbb, aA, bB, ababab], 6)
 H = nx.DiGraph()
 
 elements = [identity]
 
-non_e_elements = G.yield_elems_of_quotient(4, 12)
+non_e_elements = G.yield_elems_of_quotient(4, 6)
 for entry in non_e_elements:
     elements.append(entry)
 
@@ -66,7 +66,7 @@ for member in elements:
         elem_with_letter.add_word(member)
         elem_with_letter.add_letter(gen_letter)
         for member_2 in elements:
-            if G.test_equals(elem_with_letter, member_2, 12):
+            if G.test_equals(elem_with_letter, member_2, 6):
                 H.add_edges_from([(member, member_2)], color=color_list[j])
                 break
 
@@ -88,7 +88,7 @@ pos = nx.spring_layout(H)
 
 nx.draw(H, pos, **options, labels=word_labels, edge_color=colors)
 
-numbers = open("products/g4_4_12.txt", 'w')
+numbers = open("products/g4_4_6.txt", 'w')
 
 numbers.write(str(H.number_of_nodes()) + " is the number of nodes\n")
 numbers.write(str(H.number_of_edges()) + " is the number of edges\n")
@@ -100,6 +100,6 @@ numbers.close()
 #     print(node)
 # for (u, v) in edges:
 #     print(u.return_word_str(), v.return_word_str())
-imagename = "products/g4_4_12.png"
+imagename = "products/g4_4_6.png"
 
 plt.savefig(imagename)
