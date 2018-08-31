3e3d50ede5d7610326771085c2a9e4d7e9018a3d
natebook <ndombrowski20@gmail.com>
2018-08-31 11:31:08 -0400

    11:30
    scratch_paper is being re-purposed in order to generate g3 4_6

--- scratch_paper.py
+++ scratch_paper.py
@@ -1,30 +1,105 @@
-from master_file_cayley import Word, NewGroup, Letter
-from whiteboard import Cayley
+from master_file_cayley import Letter
+from master_file_cayley import Word
+from master_file_cayley import NewGroup
+import os
 import psutil
+import networkx as nx
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+# import pickle
+import time
 
-a = Letter("a", 1)
-b = Letter("b", 1)
-c = Letter("c", 1)
 
-A = Letter("a", -1)
-B = Letter("b", -1)
-C = Letter("c", -1)
+process = psutil.Process(os.getpid())
+start_time = time.time()
 
-aA = Word([a, A])
-bB = Word([b, B])
-cC = Word([c, C])
+a = Letter("a")
+b = Letter("b")
+A = Letter("A")
+B = Letter("B")
+
+identity = Word([])
+
+a_word = Word([a])
+b_word = Word([b])
 
 aa = Word([a, a])
+aA = Word([a, A])
+bB = Word([b, B])
 bb = Word([b, b])
-cc = Word([c, c])
-
+ab = Word([a, b])
 abAB = Word([a, b, A, B])
-acAC = Word([a, c, A, C])
-bcBC = Word([b, c, B, C])
+aaaa = Word([a, a, a, a])
+bbbbbb = Word([b, b, b, b, b, b])
+bbb = Word([b, b, b])
+ababab = Word([a, b, a, b, a, b])
+abab = Word([a, b, a, b])
+aB = Word([a, B])
+aBa = Word([a, B, a])
+baB = Word([b, a, B])
+Bab = Word([B, a, b])
+aba = Word([a, b, a])
+Ba = Word([B, a])
+B_word = Word([B])
+ba = Word([b, a])
 
-G = NewGroup([a, b, c], [aA, bB, cC, aa, bb, cc, abAB, acAC, bcBC], 10)
+G = NewGroup([a, b], [aa, bbbbbb, aA, bB, abab], 6)
+H = nx.DiGraph()
 
-newCayley = Cayley()
-newCayley.read_newgraph(G)
+elements = [identity]
 
-newCayley.draw()
+non_e_elements = G.yield_elems_of_quotient(4, 6)
+for entry in non_e_elements:
+    elements.append(entry)
+
+color_list = ['b', 'r', 'c', 'm', 'y', 'k']
+
+i = 0
+
+generators = G.list_non_inv_generators()
+
+for member in elements:
+    for j in range(len(generators)):
+        gen_letter = generators[j]
+        elem_with_letter = Word([])
+        elem_with_letter.add_word(member)
+        elem_with_letter.add_letter(gen_letter)
+        for member_2 in elements:
+            if G.test_equals(elem_with_letter, member_2, 6):
+                H.add_edges_from([(member, member_2)], color=color_list[j])
+                break
+
+    i = i + 1
+
+nodes = H.nodes()
+word_labels = {}
+for i in nodes:
+    word_labels[i] = i.return_word_str()
+
+edges = H.edges()
+colors = [H[u][v]['color'] for u, v in edges]
+
+options = {
+    'node_color': 'yellow',
+    'node_size': 400,
+}
+pos = nx.spring_layout(H)
+
+nx.draw(H, pos, **options, labels=word_labels, edge_color=colors)
+
+numbers = open("products/g3_4_6.txt", 'w')
+
+numbers.write(str(H.number_of_nodes()) + " is the number of nodes\n")
+numbers.write(str(H.number_of_edges()) + " is the number of edges\n")
+numbers.write(str("Run time = " + str(time.time() - start_time)))
+
+numbers.close()
+
+# for node in nodes:
+#     print(node)
+# for (u, v) in edges:
+#     print(u.return_word_str(), v.return_word_str())
+imagename = "products/g3_4_6.png"
+
+plt.savefig(imagename)
