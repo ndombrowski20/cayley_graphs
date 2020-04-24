c8f7403ca7356f7aa493f6465d0c5fe512522d83
Nate Dombrowski <ndombrowski20@gmail.com>
2020-04-24 01:49:14 -0500

    added the full data set and renamed the smaller one to a more appropriate title. I removed the reg_grad_step function as well considering it only diverged

--- ANNs/project/monkey_biz.py
+++ ANNs/project/monkey_biz.py
@@ -4,6 +4,7 @@ import torch.optim as optim
 import numpy as np
 import csv
 import pandas as pd
+import time as time
 
 
 # this is our x and y data
@@ -11,16 +12,15 @@ i = 0
 X = np.arange(0, 25)
 Y = np.array([])
 
-with open('data_full.csv', newline='') as csvfile:
+start = time.time()
+
+with open('data_small.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_NONNUMERIC)
     for row in reader:
         X = np.vstack((X, row[:-1]))
-        Y = np.append(Y, float(row[-1]))
+        Y = np.append(Y, [row[-1]])
 
 X = np.delete(X, 0, 0)
-
-print(X.shape)
-print(Y.shape)
 
 X = tch.tensor(X).float()
 Y = tch.tensor(Y).float()
@@ -36,7 +36,7 @@ print("size of X=",X.shape)
 
 # Now we set up the SGD
 
-optimizer = optim.SGD(MyLinearNet.parameters(), lr=0.0001)
+optimizer = optim.SGD(MyLinearNet.parameters(), lr=.00000000000005005)
 
 # This defines the loss function as MSE
 
@@ -44,38 +44,21 @@ MyLossFun = nn.MSELoss()
 
 # and here we go
 
-
 def zero_grad_step(optim, X, Y, MLN):
     L = tch.tensor(100)
-    print(L)
-    print(L.item())
     i = 0
-    while L > 0.01 and i < 100:
+    L_trend = []
+    while L > 0.1 and i < 1000:
         i += 1
         optim.zero_grad()
         Yhat = MLN(X)
-        print(Yhat.shape)
-        print(Yhat)
-        print(Y.shape)
         L = MyLossFun(Yhat, Y)
-        print(L.item())
+        L_trend.append(L.item())
         L.backward()
         optim.step()
-        print(i)
+    print(L_trend)
 
 
 zero_grad_step(optimizer, X, Y, MyLinearNet)
 
-
-def reg_grad_step(optimizer, X, Y, MyLinearNet):
-  L=tch.tensor(100)
-  i=0
-  while L>0.01 and i<100:
-    i+=1
-    Yhat=MyLinearNet(X)
-    L = MyLossFun(Yhat, Y)
-    print(L.item())
-    L.backward()
-    optimizer.step()
-
-# reg_grad_step(optimizer, X, Y, MyLinearNet)
+print(time.time() - start)
